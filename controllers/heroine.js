8fb3b18f63add4a6fb35d0717444e11b159e18ac
Adithyakrishna9 <98135664+Adithyakrishna9@users.noreply.github.com>
2022-04-14 21:42:22 -0500

    Updated resolved issues

--- controllers/heroine.js
+++ controllers/heroine.js
@@ -11,9 +11,19 @@ exports.heroine_list = async function (req, res) {
     }
 };
 // for a specific heroine.
-exports.heroine_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Heroine detail: ' + req.params.id);
-};
+// exports.heroine_detail = function (req, res) {
+//     res.send('NOT IMPLEMENTED: Heroine detail: ' + req.params.id);
+// };
+exports.heroine_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Heroine.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle Heroine create on POST.
 exports.heroine_create_post = async function (req, res) {
     console.log(req.body)
@@ -39,9 +49,29 @@ exports.heroine_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Heroine delete DELETE ' + req.params.id);
 };
 // Handle heroine update form on PUT.
-exports.heroine_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Heroine update PUT' + req.params.id);
-};
+// exports.heroine_update_put = function (req, res) {
+//     res.send('NOT IMPLEMENTED: Heroine update PUT' + req.params.id);
+// };
+// Handle Costume update form on PUT. 
+exports.heroine_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Heroine.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.heroine_name)  
+               toUpdate.heroine_name = req.body.heroine_name; 
+        if(req.body.heroine_cost) toUpdate.heroine_cost = req.body.heroine_cost; 
+        if(req.body.heroine_color) toUpdate.heroine_color = req.body.heroine_color; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
 // VIEWS
 // Handle a show all view
