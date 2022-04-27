764f6ef8ee4307355cec37db20da8fcdbfe1bdc3
Adithyakrishna9 <98135664+Adithyakrishna9@users.noreply.github.com>
2022-04-27 13:32:56 -0500

    the last commit done including bonuses

--- models/heroine.js
+++ models/heroine.js
@@ -1,7 +1,28 @@
+// const mongoose = require("mongoose")
+// const heroineSchema = mongoose.Schema({
+//     heroine_name: String,
+//     heroine_color: String,
+//     heroine_cost: Number
+// })
+// module.exports = mongoose.model("Heroine", heroineSchema)
+
 const mongoose = require("mongoose")
 const heroineSchema = mongoose.Schema({
-    heroine_name: String,
-    heroine_color: String,
-    heroine_cost: Number
+    heroine_name: { 
+        type: String,
+        minLength: 5,
+        maxLength: 20, 
+        required: true,
+        trim: true
+    },
+    heroine_color: { 
+        type: String, 
+        required: true
+    },
+    heroine_cost: { 
+        type: Number, 
+        min:[1000, 'Must be at least 1000, got {VALUE}'], 
+        max:1000000
+    }
 })
 module.exports = mongoose.model("Heroine", heroineSchema)
--- routes/heroines.js
+++ routes/heroines.js
@@ -1,7 +1,13 @@
 var express = require('express');
 const heroine_controller = require('../controllers/heroine');
 var router = express.Router();
-
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
 /* GET home page. */
 /*
 router.get('/', function(req, res, next) {
@@ -13,14 +19,15 @@ router.get('/', function(req, res, next) {
 router.get('/', heroine_controller.heroine_view_all_Page );
 
 /* GET detail heroine page */ 
-router.get('/detail', heroine_controller.heroine_view_one_Page); 
+router.get('/detail',secured, heroine_controller.heroine_view_one_Page); 
 /* GET create heroine page */
-router.get('/create', heroine_controller.heroine_create_Page);
+router.get('/create',secured, heroine_controller.heroine_create_Page);
 /* GET create update page */
-router.get('/update', heroine_controller.heroine_update_Page);
+// router.get('/update', secured, heroine_controller.heroine_update_Page);
+router.get('/update',secured,heroine_controller.heroine_update_Page); 
+ 
+// A little function to check if we have an authorized user and continue on 
+
 /* GET delete heroine page */
 router.get('/delete', heroine_controller.heroine_delete_Page);
-
-
-
 module.exports = router;
